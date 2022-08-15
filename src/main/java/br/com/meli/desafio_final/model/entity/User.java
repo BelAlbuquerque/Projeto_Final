5fa0869e3e381ff2118920f081374e5028a141a5
BelAlbuquerque <mariaisabel.albuquerque@mercadolivre.com>
2022-08-14 22:10:27 -0300

    fix: refatora entidade User

--- src/main/java/br/com/meli/desafio_final/model/entity/User.java
+++ src/main/java/br/com/meli/desafio_final/model/entity/User.java
@@ -2,10 +2,16 @@ package br.com.meli.desafio_final.model.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import javax.validation.constraints.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Getter
@@ -13,7 +19,7 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 @Inheritance(strategy = InheritanceType.JOINED)
-public class User {
+public class User implements UserDetails {
         @Id
         @Column(unique = true, nullable = false)
         @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -39,4 +45,44 @@ public class User {
         @NotEmpty(message = "o campo 'password' não pode ser vazio")
         private String password;
 
- }
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+                List<SimpleGrantedAuthority> authorities = new ArrayList<SimpleGrantedAuthority>();
+                authorities.add(new SimpleGrantedAuthority(this.role));
+                return authorities;
+        }
+
+        public long getId() { return this.id; }
+
+        public String getRole() { return this.role; }
+
+        @Override
+        public String getPassword() {
+                return this.password;
+        }
+
+        @Override
+        public String getUsername() {
+                return this.email;
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+                return true;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+                return true;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+                return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+                return true;
+        }
+}
